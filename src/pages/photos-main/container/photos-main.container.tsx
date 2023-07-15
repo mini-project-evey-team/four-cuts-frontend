d9f427c89466d21b666731b05f1c0ed9d94f93ca
Haru Im <chalk323@naver.com>
2023-07-15 22:53:23 +0900

    feat: add views to main container

--- src/pages/photos-main/container/photos-main.container.tsx
+++ src/pages/photos-main/container/photos-main.container.tsx
@@ -1,17 +1,27 @@
 import { FC } from "react";
-import { HeaderView } from "../views";
+import {
+  PhotosMainHeaderView,
+  PhotosMainSlideView,
+  PhotosMainTitleView,
+} from "../views";
 import { Layout } from "../../../styles";
-import { Link } from "react-router-dom";
 
 type IPhotosMainContainerProps = {};
 
 export const PhotosMainContainer: FC<IPhotosMainContainerProps> = ({}) => {
   return (
     <Layout>
-      <HeaderView />
-      <Link to="/list">to list page</Link>
-      <Link to="/post">to post page</Link>
-      <Link to="/list/:id/detail">detail page</Link>
+      <PhotosMainHeaderView />
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        <PhotosMainTitleView />
+        <PhotosMainSlideView />
+      </div>
     </Layout>
   );
 };
