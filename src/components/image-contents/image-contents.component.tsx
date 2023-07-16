16d52d5b1892c6acbbff73f7cebf7cfb5302230e
Haru Im <chalk323@naver.com>
2023-07-16 16:09:56 +0900

    css: fix css

--- src/components/image-contents/image-contents.component.tsx
+++ src/components/image-contents/image-contents.component.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { ImageFrameComponent } from "../image-frame";
 import styled from "styled-components";
+
 type IImageContentsComponentProps = {
   fourPhotos: {
     id: string;
@@ -17,26 +18,37 @@ export const ImageContentsComponent: FC<IImageContentsComponentProps> = ({
 }) => {
   const { title, imageUrl } = fourPhotos;
   return (
-    <div
-      style={{
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ImageFrameComponent title={title} width={width} fontSize="20px">
-        {imageUrl.map((e, i) => {
-          return <StyledImage src={e} key={i} />;
-        })}
-      </ImageFrameComponent>
-    </div>
+    <StyledOuterDiv>
+      <StyledInnerDiv>
+        <ImageFrameComponent title={title} width={width} fontSize="20px">
+          {imageUrl.map((e, i) => {
+            return <StyledImage src={e} key={i} />;
+          })}
+        </ImageFrameComponent>
+      </StyledInnerDiv>
+    </StyledOuterDiv>
   );
 };
+
+const StyledOuterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledInnerDiv = styled.div`
+  transition: box-shadow 0.2s ease-in-out, transform 0.2s ease-in-out;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3), 0 0px 0px rgba(0, 0, 0, 0);
+
+  &:hover {
+    transform: translateY(-7px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5), 0 0px 0px rgba(0, 0, 0, 0);
+  }
+`;
 
 const StyledImage = styled.img`
   width: 50%;
   height: 50%;
-  objectfit: cover;
+  object-fit: cover;
   padding: 2px;
 `;
--- src/styles/globalLayout.tsx
+++ src/styles/globalLayout.tsx
@@ -4,6 +4,5 @@ export const Layout = styled.div`
   display: flex;
   height: 100vh;
   width: 100%;
-  padding: 20;
   flex-direction: column;
 `;
